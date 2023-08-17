08a665510dc9734a2a63d7a87f0a7b1566fe3656
Bruno Paz <brunopaz00@yahoo.com.mx>
2023-08-17 11:44:01 -0600

    Added cards

---
+++ src/components/PortfolioComp.vue
@@ -0,0 +1,172 @@
+<template>
+    <div style="width: 100%;">
+      <div class="text-h3 text-bold text-center">Portfolio</div>
+
+      <div class="row" style="width: 100%;">
+        <div id="parent" class="fit row wrap justify-around items-start content-start" style="overflow: hidden;">
+
+          <div class="q-mt-md" style="overflow: auto;">
+            <q-card flat bordered >
+              <q-card-section >
+                <q-img
+                  src="../../public/bolsa_uaq.svg"
+                  class="image"
+                  fit="contain"
+                />
+                <div class="text-h4 text-bold q-mt-sm">Bolsa Uaq</div>
+                <div class="text-subtitle1">Website</div>
+                <div class="text-subtitle1">Rest API</div>
+                <a style="color: #0038FF;" target="_blank" href="https://bolsa.fif-uaq.mx" class="my-custom-link">https://bolsa.fif-uaq.mx</a>
+              </q-card-section>
+              <q-card-section >
+                <q-separator class="q-mt-sm"></q-separator>
+                <div class="flex q-gutter-md q-my-sm">
+                  <div class="gray-circle q-pa-sm">VueJs</div>
+                  <div class="gray-circle q-pa-sm">Quasar</div>
+                  <div class="gray-circle q-pa-sm">ExpressJs</div>
+                  <div class="gray-circle q-pa-sm">MySQL</div>
+                  <div class="gray-circle q-pa-sm">Sequelize</div>
+                  <div class="gray-circle q-pa-sm">MVC</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+
+
+          <div  class="q-mt-md" style="overflow: auto;">
+            <q-card flat bordered >
+              <q-card-section >
+                <div>
+                  <q-img
+                    src="../../public/indereq_web.svg"
+                    class="image"
+                    fit="contain"
+                  />
+                  <q-img
+                    src="../../public/indereq_mobile.svg"
+                    class="mobile-image"
+                    fit="contain"
+                  />
+                </div>
+                <div class="text-h4 text-bold q-mt-sm">Indereq</div>
+                <div class="text-subtitle1">Website</div>
+                <div class="text-subtitle1">Rest API</div>
+                <div class="text-subtitle1">Native IOS/Android mobile app</div>
+                <a style="color: #0038FF;" target="_blank" href="https://indirec-web.vercel.app" class="my-custom-link">https://indirec-web.vercel.app</a>
+              </q-card-section>
+              <q-card-section >
+                <q-separator class="q-mt-sm"></q-separator>
+                <div class="flex q-gutter-md q-my-sm">
+                  <div class="gray-circle q-pa-sm">React</div>
+                  <div class="gray-circle q-pa-sm">React Native</div>
+                  <div class="gray-circle q-pa-sm">Feathers</div>
+                  <div class="gray-circle q-pa-sm">ExpressJs</div>
+                  <div class="gray-circle q-pa-sm">MySQL</div>
+                  <div class="gray-circle q-pa-sm">Sequelize</div>
+                  <div class="gray-circle q-pa-sm">MVC</div>
+                  <div class="gray-circle q-pa-sm">AWS</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+
+
+          <div class="q-mt-md" style="overflow: auto;">
+            <q-card flat bordered >
+              <q-card-section >
+                <div>
+                  <q-img
+                    src="../../public/cosecha_login.svg"
+                    class="mobile-image"
+                    fit="contain"
+                  />
+                  <q-img
+                    src="../../public/cosecha_reparto.svg"
+                    class="mobile-image"
+                    fit="contain"
+                  />
+                </div>
+                <div class="text-h4 text-bold q-mt-sm">Cosecha colectiva</div>
+                <div class="text-subtitle1">Native IOS/Android mobile app</div>
+                <div class="text-subtitle1">API</div>
+                <a style="color: #0038FF;" target="_blank" href="https://indirec-web.vercel.app" class="my-custom-link">https://indirec-web.vercel.app</a>
+              </q-card-section>
+              <q-card-section >
+                <q-separator class="q-mt-sm"></q-separator>
+                <div class="flex q-gutter-md q-my-sm">
+                  <div class="gray-circle q-pa-sm">React</div>
+                  <div class="gray-circle q-pa-sm">React Native</div>
+                  <div class="gray-circle q-pa-sm">Feathers</div>
+                  <div class="gray-circle q-pa-sm">ExpressJs</div>
+                  <div class="gray-circle q-pa-sm">MySQL</div>
+                  <div class="gray-circle q-pa-sm">Sequelize</div>
+                  <div class="gray-circle q-pa-sm">MVC</div>
+                  <div class="gray-circle q-pa-sm">AWS</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+
+
+          <div class="q-mt-md" style="overflow: auto;">
+            <q-card flat bordered >
+              <q-card-section >
+                <div>
+                  <q-img
+                    src="../../public/indereq_web.svg"
+                    class="image"
+                    fit="contain"
+                  />
+                  <q-img
+                    src="../../public/indereq_mobile.svg"
+                    class="mobile-image"
+                    fit="contain"
+                  />
+                </div>
+                <div class="text-h4 text-bold q-mt-sm">Agenda UAQ</div>
+                <div class="text-subtitle1">Website</div>
+                <div class="text-subtitle1">Rest API</div>
+                <div class="text-subtitle1">Native IOS/Android mobile app</div>
+                <a style="color: #0038FF;" target="_blank" href="https://indirec-web.vercel.app" class="my-custom-link">https://indirec-web.vercel.app</a>
+              </q-card-section>
+              <q-card-section >
+                <q-separator class="q-mt-sm"></q-separator>
+                <div class="flex q-gutter-md q-my-sm">
+                  <div class="gray-circle q-pa-sm">React</div>
+                  <div class="gray-circle q-pa-sm">React Native</div>
+                  <div class="gray-circle q-pa-sm">Feathers</div>
+                  <div class="gray-circle q-pa-sm">ExpressJs</div>
+                  <div class="gray-circle q-pa-sm">MySQL</div>
+                  <div class="gray-circle q-pa-sm">Sequelize</div>
+                  <div class="gray-circle q-pa-sm">MVC</div>
+                  <div class="gray-circle q-pa-sm">AWS</div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+  
+  export default defineComponent({
+    name: 'PortfolioComp',
+  })
+</script>
+  
+<style>
+
+.image{
+  width: 650px;
+  height: 300px;
+}
+
+.mobile-image{
+  width: 150px;
+  height: 300px;
+}
+
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,128 @@
+<template>
+  <q-page padding class="column flex-center">
+    <PortfolioComp/>
+    <div >
+      <p>Skills</p>
+      <q-card flat bordered class="border-black">
+        <q-card-section class="column flex-center">
+          <q-icon name="settings_accessibility" size="30px" />
+          <div>Leadership</div>
+          <div class="text-justify" style="max-width: 500px;">During my time on Centro de Desarrollo i got the opportunity of leading some of the projects , where i faced the difficulties of managing people. At the end i learned how to manage people according to his profile (skills and preferences) to get the best of them</div>
+        </q-card-section>
+      </q-card>
+      <q-card flat bordered class="border-black border-radius">
+        <q-card-section class="flex q-gutter-md">
+          <div >JavaScript</div>
+          <div class="black-dot"></div>
+          <div >VueJs</div>
+          <div class="black-dot"></div>
+          <div >ExpressJs</div>
+        </q-card-section>
+      </q-card>
+    </div>
+    <div>
+      <p>About me</p>
+      <div class="row" >
+
+        <div class="q-col" style="width: 50%;">
+          <div style="width: 10vw;height: 10vh;border-radius: 50px;"></div>
+          <div>
+            <q-icon size="30px" name="language"/>
+            <p>Languages</p>
+          </div>
+          <div>
+            <p>Spanish (native)</p>
+            <p>English (B-2)</p>
+          </div>
+        </div>
+
+        <div class="q-col" style="width: 50%;">
+          <p>Full stack developer</p>
+          <p>During my time on Centro de Desarrollo i got the opportunity of leading some of the projects , where i faced the difficulties of managing people. At the end i learned how to manage people according to his profile (skills and preferences) to get the best of them</p>
+          <div>
+            <q-icon size="30px" name="person"/>
+            <p>Personal data</p>
+          </div>
+          <q-separator
+            size="1px"
+            color="black"
+            class="q-mx-lg q-mx-sm q-my-none"
+          />
+          <div class="col">
+            <div class="row">
+              <div>
+                <p>Name:</p>
+                <p>Bruno Ivan Paz Martinez</p>
+              </div>
+              <div>
+                <p>Residence:</p>
+                <p>Queretaro-mexico</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div>
+                <p>Github:</p>
+                <p>www.github.com</p>
+              </div>
+              <div>
+                <p>Linkedin:</p>
+                <p>www.linkedin.com</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div>
+      <q-card flat bordered class="border-black">
+        <q-card-section class="column flex-center">
+          <q-icon name="call" size="30px" />
+          <div class="text-justify" style="max-width: 500px;">(+52) 442 669 6054</div>
+        </q-card-section>
+      </q-card>
+      <q-card flat bordered class="border-black">
+        <q-card-section class="column flex-center">
+          <q-icon name="mail" size="30px" />
+          <div class="text-justify" style="max-width: 500px;">brunopaz00@yahoo.com.mx</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import PortfolioComp from '../components/PortfolioComp.vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  components:{
+    PortfolioComp
+  }
+})
+</script>
+
+<style>
+.black-dot{
+  width: 15px;
+  height: 15px;
+  background-color: black;
+  border-radius: 20px;
+}
+.border-black{
+  border-color: black;
+}
+.border-radius{
+  border-radius: 10px;
+}
+.gray-circle{
+  background-color: #E4E4E4;
+  width: fit-content;
+  border-radius: 15px;
+}
+</style>
