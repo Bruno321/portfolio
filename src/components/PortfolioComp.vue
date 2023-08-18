857743a826906f09fb4f669a2d5ecb2cc378f93d
Bruno Paz <brunopaz00@yahoo.com.mx>
2023-08-17 23:54:05 -0600

    Added responsive cards

--- src/components/PortfolioComp.vue
+++ src/components/PortfolioComp.vue
@@ -1,24 +1,20 @@
 <template>
     <div style="width: 100%;">
-      <div class="text-h3 text-bold text-center">Portfolio</div>
+      <div class="text-h3 text-bold text-center q-mt-lg">Portfolio</div>
 
-      <div class="row" style="width: 100%;">
-        <div id="parent" class="fit row wrap justify-around items-start content-start" style="overflow: hidden;">
-
-          <div class="q-mt-md" style="overflow: auto;">
-            <q-card flat bordered >
-              <q-card-section >
-                <q-img
-                  src="../../public/bolsa_uaq.svg"
-                  class="image"
-                  fit="contain"
-                />
+      <div class="row q-mt-lg" >
+        <div class="col-xs-12 col-sm-12 col-md-6 q-px-xl q-py-sm">
+          <q-card flat bordered >
+              <q-card-section>
+                  <q-img
+                    src="bolsa_uaq.jpg"
+                  />
                 <div class="text-h4 text-bold q-mt-sm">Bolsa Uaq</div>
                 <div class="text-subtitle1">Website</div>
                 <div class="text-subtitle1">Rest API</div>
                 <a style="color: #0038FF;" target="_blank" href="https://bolsa.fif-uaq.mx" class="my-custom-link">https://bolsa.fif-uaq.mx</a>
               </q-card-section>
-              <q-card-section >
+              <q-card-section style="margin-top: 26px;">
                 <q-separator class="q-mt-sm"></q-separator>
                 <div class="flex q-gutter-md q-my-sm">
                   <div class="gray-circle q-pa-sm">VueJs</div>
@@ -30,24 +26,14 @@
                 </div>
               </q-card-section>
             </q-card>
-          </div>
+        </div>
 
-
-          <div  class="q-mt-md" style="overflow: auto;">
-            <q-card flat bordered >
+        <div class="col-xs-12 col-sm-12 col-md-6 q-px-xl q-py-sm">
+          <q-card flat bordered>
               <q-card-section >
-                <div>
-                  <q-img
-                    src="../../public/indereq_web.svg"
-                    class="image"
-                    fit="contain"
-                  />
-                  <q-img
-                    src="../../public/indereq_mobile.svg"
-                    class="mobile-image"
-                    fit="contain"
-                  />
-                </div>
+                <q-img
+                  src="indereq_web.jpg"
+                />
                 <div class="text-h4 text-bold q-mt-sm">Indereq</div>
                 <div class="text-subtitle1">Website</div>
                 <div class="text-subtitle1">Rest API</div>
@@ -68,22 +54,19 @@
                 </div>
               </q-card-section>
             </q-card>
-          </div>
+        </div>
 
-
-          <div class="q-mt-md" style="overflow: auto;">
-            <q-card flat bordered >
+        <div class="col-xs-12 col-sm-12 col-md-6 q-px-xl q-py-xl">
+          <q-card flat bordered>
               <q-card-section >
-                <div>
+                <div style="width: 100%;height: 500px;display: flex;flex-direction: row;">
                   <q-img
-                    src="../../public/cosecha_login.svg"
-                    class="mobile-image"
-                    fit="contain"
+                    src="cosecha_info.jpg"
+                    fit="scale-down"
                   />
                   <q-img
-                    src="../../public/cosecha_reparto.svg"
-                    class="mobile-image"
-                    fit="contain"
+                    src="cosecha_grupo.jpg"
+                    fit="scale-down"
                   />
                 </div>
                 <div class="text-h4 text-bold q-mt-sm">Cosecha colectiva</div>
@@ -94,59 +77,52 @@
               <q-card-section >
                 <q-separator class="q-mt-sm"></q-separator>
                 <div class="flex q-gutter-md q-my-sm">
-                  <div class="gray-circle q-pa-sm">React</div>
                   <div class="gray-circle q-pa-sm">React Native</div>
-                  <div class="gray-circle q-pa-sm">Feathers</div>
                   <div class="gray-circle q-pa-sm">ExpressJs</div>
                   <div class="gray-circle q-pa-sm">MySQL</div>
                   <div class="gray-circle q-pa-sm">Sequelize</div>
                   <div class="gray-circle q-pa-sm">MVC</div>
                   <div class="gray-circle q-pa-sm">AWS</div>
+                  <div class="gray-circle q-pa-sm">TypeScript</div>
                 </div>
               </q-card-section>
             </q-card>
-          </div>
+        </div>
 
-
-          <div class="q-mt-md" style="overflow: auto;">
-            <q-card flat bordered >
+        <div class="col-xs-12 col-sm-12 col-md-6 q-px-xl q-py-xl">
+          <q-card flat bordered>
               <q-card-section >
-                <div>
+                <div style="width: 100%;height: 500px;display: flex;flex-direction: row;">
                   <q-img
-                    src="../../public/indereq_web.svg"
-                    class="image"
-                    fit="contain"
+                    src="agenda_login.jpg"
+                    fit="scale-down"
                   />
                   <q-img
-                    src="../../public/indereq_mobile.svg"
-                    class="mobile-image"
-                    fit="contain"
+                    src="agenda_noti.jpg"
+                    fit="scale-down"
                   />
                 </div>
+
                 <div class="text-h4 text-bold q-mt-sm">Agenda UAQ</div>
-                <div class="text-subtitle1">Website</div>
-                <div class="text-subtitle1">Rest API</div>
                 <div class="text-subtitle1">Native IOS/Android mobile app</div>
                 <a style="color: #0038FF;" target="_blank" href="https://indirec-web.vercel.app" class="my-custom-link">https://indirec-web.vercel.app</a>
               </q-card-section>
-              <q-card-section >
+              <q-card-section style="margin-top: 26px;">
                 <q-separator class="q-mt-sm"></q-separator>
                 <div class="flex q-gutter-md q-my-sm">
-                  <div class="gray-circle q-pa-sm">React</div>
                   <div class="gray-circle q-pa-sm">React Native</div>
-                  <div class="gray-circle q-pa-sm">Feathers</div>
-                  <div class="gray-circle q-pa-sm">ExpressJs</div>
-                  <div class="gray-circle q-pa-sm">MySQL</div>
-                  <div class="gray-circle q-pa-sm">Sequelize</div>
-                  <div class="gray-circle q-pa-sm">MVC</div>
-                  <div class="gray-circle q-pa-sm">AWS</div>
+                  <div class="gray-circle q-pa-sm">Push Notifications</div>
+                  <div class="gray-circle q-pa-sm">Firebase Cloud Messaging </div>
                 </div>
               </q-card-section>
             </q-card>
-          </div>
         </div>
+
+        
       </div>
+
     </div>
+
 </template>
 
 <script>
@@ -158,15 +134,5 @@
 </script>
   
 <style>
-
-.image{
-  width: 650px;
-  height: 300px;
-}
-
-.mobile-image{
-  width: 150px;
-  height: 300px;
-}
 
 </style>
